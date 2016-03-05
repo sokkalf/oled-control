3b27b75d460be83469546fab3bdfa3609556d6e9
Christian Lønaas <christian.lonaas@discombobulator.org>
2016-03-05 23:39:34 +0100

    Test display function

--- main.cpp
+++ main.cpp
@@ -50,6 +50,10 @@ bool set_cursor_position(uint8_t col, uint8_t row) {
     return i2c_write_byte(CMD, 0x80 | (col + line_offsets[row]));
 }
 
+bool clear_display() {
+    return i2c_write_byte(CMD, 0x01);
+}
+
 bool configure_display(const char* bus, uint8_t address) {
     i2c_bus = bus;
     oled_address = address;
@@ -102,12 +106,75 @@ bool init_display() {
     i2c_write_byte(CMD, 0x01);
     i2c_write_byte(CMD, 0x80);
     i2c_write_byte(CMD, 0x0c);
+    usleep(1000); // spec says to hold on for 0.1 secs before data
+}
+
+void test_display() {
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "In A.D. 2101");
+    set_cursor_position(1, 2);
+    i2c_write_bytes(DATA, "War was beginning.");
+    sleep(5);
+    clear_display();
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "Captain:");
+    set_cursor_position(4, 2);
+    i2c_write_bytes(DATA, "What happen?");
+    sleep(2);
+    clear_display();
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "Mechanic:");
+    set_cursor_position(0, 2);
+    i2c_write_bytes(DATA, "Somebody set up us");
+    set_cursor_position(0, 3);
+    i2c_write_bytes(DATA, "the bomb");
+    sleep(2);
+    clear_display();
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "Operator:");
+    set_cursor_position(0, 2);
+    i2c_write_bytes(DATA, "We get signal.");
+    sleep(2);
+    clear_display();
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "Captain:");
+    set_cursor_position(0, 2);
+    i2c_write_bytes(DATA, "WHAT !");
+    sleep(2);
+    clear_display();
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "Operator:");
+    set_cursor_position(1, 2);
+    i2c_write_bytes(DATA, "Main screen");
+    set_cursor_position(1, 3);
+    i2c_write_bytes(DATA, "turn on.");
+    sleep(2);
+    clear_display();
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "Captain:");
+    set_cursor_position(0, 2);
+    i2c_write_bytes(DATA, "It's you !!");
+    sleep(2);
+    clear_display();
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "Cats:");
+    set_cursor_position(0, 2);
+    i2c_write_bytes(DATA, "How are you");
+    set_cursor_position(1, 3);
+    i2c_write_bytes(DATA, "gentlemen !!");
+    sleep(2);
+    clear_display();
+    set_cursor_position(2, 1);
+    i2c_write_bytes(DATA, "Cats:");
+    set_cursor_position(0, 2);
+    i2c_write_bytes(DATA, "All your base are");
+    set_cursor_position(1, 3);
+    i2c_write_bytes(DATA, "belong to us.");
 }
 
 int main() {
     configure_display("/dev/i2c-1", 0x3c);
     init_display();
-    set_cursor_position(4, 2);
-    i2c_write_bytes(DATA, "Heisann hoppsan!");
+    test_display();
     return 0;
 }
