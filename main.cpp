85d42e8b3fa3b93858817f8d13bf3619e51f1299
Christian Lønaas <christian.lonaas@discombobulator.org>
2016-03-06 00:13:25 +0100

    Better error handling

--- main.cpp
+++ main.cpp
@@ -72,40 +72,43 @@ bool configure_display(const char* bus, uint8_t address) {
 }
 
 bool init_display() {
-    i2c_write_byte(CMD, 0x2a); // **** Set "RE"=1<--->00101010B
-    i2c_write_byte(CMD, 0x71);
-    i2c_write_byte(CMD, 0x5c);
-    i2c_write_byte(CMD, 0x28);
-    i2c_write_byte(CMD, 0x08); // display off
-    i2c_write_byte(CMD, 0x2a);
-    i2c_write_byte(CMD, 0x79);
-    i2c_write_byte(CMD, 0xd5);
-    i2c_write_byte(CMD, 0x70);
-    i2c_write_byte(CMD, 0x78);
-    i2c_write_byte(CMD, 0x09);
-    i2c_write_byte(CMD, 0x06);
-    i2c_write_byte(CMD, 0x72);
-    i2c_write_byte(DATA, 0x00);
-    i2c_write_byte(CMD, 0x2a);
-    i2c_write_byte(CMD, 0x79);
-    i2c_write_byte(CMD, 0xda);
-    i2c_write_byte(CMD, 0x10);
-    i2c_write_byte(CMD, 0xdc);
-    i2c_write_byte(CMD, 0x00);
-    i2c_write_byte(CMD, 0x81); // set contrast
-    i2c_write_byte(CMD, 0x50); // contrast level
-    i2c_write_byte(CMD, 0xdb);
-    i2c_write_byte(CMD, 0x30);
-    i2c_write_byte(CMD, 0xdc);
-    i2c_write_byte(CMD, 0x03);
-    i2c_write_byte(CMD, 0x78);
-    i2c_write_byte(CMD, 0x28);
-    i2c_write_byte(CMD, 0x2a);
-    i2c_write_byte(CMD, 0x06);
-    i2c_write_byte(CMD, 0x28);
-    i2c_write_byte(CMD, 0x01);
-    i2c_write_byte(CMD, 0x80);
-    i2c_write_byte(CMD, 0x0c);
+    if(!configured)
+        return false;
+
+    return i2c_write_byte(CMD, 0x2a) && // **** Set "RE"=1<--->00101010B
+    i2c_write_byte(CMD, 0x71) &&
+    i2c_write_byte(CMD, 0x5c) &&
+    i2c_write_byte(CMD, 0x28) &&
+    i2c_write_byte(CMD, 0x08) && // display off
+    i2c_write_byte(CMD, 0x2a) &&
+    i2c_write_byte(CMD, 0x79) &&
+    i2c_write_byte(CMD, 0xd5) &&
+    i2c_write_byte(CMD, 0x70) &&
+    i2c_write_byte(CMD, 0x78) &&
+    i2c_write_byte(CMD, 0x09) &&
+    i2c_write_byte(CMD, 0x06) &&
+    i2c_write_byte(CMD, 0x72) &&
+    i2c_write_byte(DATA, 0x00) &&
+    i2c_write_byte(CMD, 0x2a) &&
+    i2c_write_byte(CMD, 0x79) &&
+    i2c_write_byte(CMD, 0xda) &&
+    i2c_write_byte(CMD, 0x10) &&
+    i2c_write_byte(CMD, 0xdc) &&
+    i2c_write_byte(CMD, 0x00) &&
+    i2c_write_byte(CMD, 0x81) && // set contrast
+    i2c_write_byte(CMD, 0x50) && // contrast level
+    i2c_write_byte(CMD, 0xdb) &&
+    i2c_write_byte(CMD, 0x30) &&
+    i2c_write_byte(CMD, 0xdc) &&
+    i2c_write_byte(CMD, 0x03) &&
+    i2c_write_byte(CMD, 0x78) &&
+    i2c_write_byte(CMD, 0x28) &&
+    i2c_write_byte(CMD, 0x2a) &&
+    i2c_write_byte(CMD, 0x06) &&
+    i2c_write_byte(CMD, 0x28) &&
+    i2c_write_byte(CMD, 0x01) &&
+    i2c_write_byte(CMD, 0x80) &&
+    i2c_write_byte(CMD, 0x0c) &&
     usleep(1000); // spec says to hold on for 0.1 secs before data
 }
 
